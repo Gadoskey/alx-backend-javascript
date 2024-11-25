132abc082baecb7b9774caa2783b32bbcf7171bf
Gadoskey <gadoski321@gmail.com>
2024-11-25 15:19:35 +0100

    1

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,7 @@
 /**
  * Prints in STDOUT the string argument.
- * @param {
- *   arg: The string to print out
- * }
+ * @param {string} arg - The string to print out
  * @author Gadoskey
- * @returns
  */
 module.exports = function displayMessage(arg) {
   process.stdout.write(arg + "\n");
