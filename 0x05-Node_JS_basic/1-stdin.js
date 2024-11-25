392272e27f1cfc69f31fcba98d140ef30e5d7472
Gadoskey <gadoski321@gmail.com>
2024-11-25 15:30:11 +0100

    taskk 2

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,20 @@
+/**
+ * Prints in STDOUT the string argument.
+ * @param {string} arg - The string to print out
+ * @author Gadoskey
+ */
+process.stdin.setEncoding("utf8");
+
+const message = "Welcome to Holberton School, what is your name?";
+process.stdout.write(message + "\n");
+
+process.stdin.on("readable", function () {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write("Your name is:" + input);
+  }
+});
+
+process.stdin.on("end", function () {
+  process.stdout.write("This important software is now closing" + "\n");
+});
