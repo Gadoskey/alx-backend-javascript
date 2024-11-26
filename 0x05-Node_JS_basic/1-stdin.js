5b95c9f6fe99d7c2197b67e607090212ec1b9756
Gadoskey <gadoski321@example.com>
2024-11-26 06:35:07 +0000

    OOK

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,15 +6,23 @@
 process.stdin.setEncoding("utf8");
 
 const message = "Welcome to Holberton School, what is your name?";
+
 process.stdout.write(message + "\n");
 
-process.stdin.on("readable", function () {
+process.stdin.on(
+"readable",
+() => {
   const input = process.stdin.read();
+
   if (input !== null) {
     process.stdout.write("Your name is: " + input);
   }
-});
+}
+);
 
-process.stdin.on("end", function () {
+process.stdin.on(
+"end",
+() => {
   process.stdout.write("This important software is now closing" + "\n");
-});
+}
+);
