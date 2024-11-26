c349e6b590e147f10dac6183400f1f0c0a16b362
Gadoskey <gadoski321@gmail.com>
2024-11-26 07:49:26 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,20 +4,23 @@
  * @author Gadoskey
  */
 process.stdin.setEncoding("utf8");
-console.log("Welcome to Holberton School, what is your name?\n");
+
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
 process.stdin.on(
 "readable",
 () => {
   const input = process.stdin.read();
 
   if (input !== null) {
-    console.log("Your name is: " + input);
+    process.stdout.write(`Your name is: ${input}`);
   }
 }
 );
+
 process.stdin.on(
 "end",
 () => {
-  console.log("This important software is now closing\n");
+  process.stdout.write("This important software is now closing\n");
 }
 );
