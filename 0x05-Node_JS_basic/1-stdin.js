73f5fd0ade4b8bcb6c883b9226ff16cee8057241
Gadoskey <gadoski321@gmail.com>
2024-11-26 07:37:45 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -23,6 +23,6 @@ process.stdin.on(
 process.stdin.on(
 "end",
 () => {
-  process.stdout.write("This important software is now closing" + "\n");
+  process.stdout.write("This important software is now closing\n");
 }
 );
