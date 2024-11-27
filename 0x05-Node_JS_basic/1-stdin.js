b5597ed1f5e7a21cf01393eabc6bae98ef48ab0b
Gadoskey <gadoski321@gmail.com>
2024-11-27 12:04:30 +0100

    fighting lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,22 +3,17 @@
  * @param {string} arg - The string to print out
  * @author Gadoskey
  */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on(
-"readable",
-() => {
+process.stdin.on('readable', () => {
   const input = process.stdin.read();
-
   if (input !== null) {
     process.stdout.write(`Your name is: ${input}`);
   }
 }
 );
 
-process.stdin.on(
-"end",
-() => {
+process.stdin.on('end', () => {
   process.stdout.write("This important software is now closing\n");
 }
 );
