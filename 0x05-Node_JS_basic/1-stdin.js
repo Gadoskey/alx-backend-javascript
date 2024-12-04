d142a121b398bd5fd39e440cdc86c3d24815f279
Gadoskey <gadoski321@gmail.com>
2024-12-04 11:15:05 +0100

    back to nodejs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,24 +1,17 @@
-/**
+/*
  * Reads from STDIN and Prints in STDOUT the string argument.
  * @param {string} arg - The string to print out
  * @author Gadoskey
  */
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on(
-  "readable",
-  () => {
-    const input = process.stdin.read();
-
-    if (input !== null) {
-      process.stdout.write(`Your name is: ${input}`);
-    }
+process.stdin.on("readable", () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
   }
-);
+});
 
-process.stdin.on(
-  "end",
-  () => {
-    process.stdout.write("This important software is now closing\n");
-  }
-);
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
